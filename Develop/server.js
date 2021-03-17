a81900e80f95dfa64aee8f53cecfd0f42eabff85
E Alexander <erick.interiano05@gmail.com>
2021-03-16 19:41:34 -0700

    Added get and post routes for new notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3500
 ;
 
-
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post('/api/characters', (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+});
 
 
 
